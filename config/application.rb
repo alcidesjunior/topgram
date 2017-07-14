4f675981489a7849826a719b6220be7e39778e5f
alcidesjunior <alcidesjunior.infor@gmail.com>
2017-07-14 13:48:25 -0300

    bug fixed

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Topgram
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    I18n.locale = :'pt-BR'
   end
 end
